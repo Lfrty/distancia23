58b0b5e2f928c9a6c40789a373cda3830c6d1ac8
Lfrty <matiasdelosriosaguilar@gmail.com>
2023-05-31 18:31:40 +0200

    Añadidos los sripts de PHP

---
+++ scripts/script1-DeLosRios.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Contiene dos funciones que realizan diferentes acciones
+ * Una que saluda al nombre introducido y le da la fecha y hora y otra que hace una resta
+ * 
+ * @author Matías de los Ríos Aguilar
+ * 
+ * @version 0.1
+ * 
+ * @category Utilidades
+ */
+
+/**
+ * Saluda al nombre introducido y le dice la fecha y hora
+ * 
+ * @param String $nombre Incluye el nombre dado por el usuario
+ * 
+ * @return void No devuelve nada, lo muestra por pantalla
+ * 
+ * @version 0.5
+ * 
+ * @see https://www.php.net/manual/en/function.date.php
+ * 
+ */
+function saluda($nombre){
+    echo "Hola ".$nombre." hoy es ". date("d-m-Y"). " y son las " . date("h:i:s");
+}
+
+
+/**
+ * Realiza una resta
+ * 
+ * @param float $minuendo Número sobre el que se va a restar una cantidad
+ * 
+ * @param float $sustraendo Número que se va a resar al minuendo
+ * 
+ * @return float Devuelve un el resultado de restar el $minuendo y sustraendo
+ * 
+ * @version 0.5
+ * 
+ */
+function resta($minuendo, $sustraendo){
+    return (float) $minuendo - (float)$sustraendo;
+}
---
+++ scripts/script2-DeLosRios.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Clase Coche
+ * Implementa un objeto del tipo Coche
+ * Permite acceder y modificar sus valores mediante getters y setters
+ * 
+ * @author Matías de los Ríos Aguilar
+ * 
+ * @version 0.1
+ * 
+ * @category Objetos
+ * 
+ */
+class Coche {
+
+    private $modelo;
+    private $puertas;
+
+    private static $numCoches = 0;
+
+    /**
+     * Crea un objeto del tipo coche
+     * 
+     * @param String $modelo Nombre del modelo coche
+     * 
+     * @param int $puertas Número de puertas del coche
+     * 
+     * @version 0.1
+     * 
+     * @return void
+     */
+    public function __construct($modelo, $puertas){
+        $this -> modelo = $modelo;
+        $this -> puertas = $puertas;
+
+        self::$numCoches++;
+    }
+
+    /**
+     * Devuelve el modelo del coche
+     * 
+     * @return String $modelo Modelo de este coche
+     * 
+     * @version 0.5
+     */
+    public function getModelo() {
+        return $this -> modelo;
+    }
+
+    /**
+     * Devuelve el número de puertas del coche
+     * 
+     * @return int $puertas Número de puertas de este coche
+     * 
+     * @version 0.5
+     */
+    public function getpuertas(){
+        return $this -> puertas;
+    }
+
+    /**
+     * Cambia el modelo del coche
+     * 
+     * @param String $modelo Nuevo modelo del coche
+     * 
+     * @return void No devuelve ningún valor
+     * 
+     * @version 0.5
+     */
+    public function setModelo($modelo){
+        $this -> modelo=$modelo;
+    }
+
+    /**
+     * Cambia el número de puertas del coche
+     * 
+     * @param int $puertas Nuevo número de puertas del coche
+     * 
+     * @return void No devuelve ningún valor
+     * 
+     * @version 0.5
+     */
+    public function setpuertas(){
+        $this -> puertas=$puertas;
+    }
+
+    /**
+     * Escribe en pantalla el número de coches creados
+     * 
+     * @return void No devuelve nada
+     * 
+     * @static Esta función es estática
+     * 
+     * @todo Se incluirá el número de modificaciones que se han hecho
+     * 
+     * @version 0.5
+     */
+    public static function cuentaCoches(){
+        echo "Número de coches creados: ". self::$numCoches;
+    }
+
+}
